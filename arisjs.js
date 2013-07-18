fb82f9d1a71b27a8473d8a6e42295da74fbd1216
Phildo <phildo211@gmail.com>
2013-07-18 11:27:50 -0500

    cleaned up and added 'exitTo' functionality

--- arisjs.js
+++ arisjs.js
@@ -31,7 +31,7 @@ var ARISJS = function()
             if(req == "aris://inventory/get/" + 99999999)
                 this.didUpdateItemQty(99999999,1);
             this.isNotCurrentlyCalling();
-            */
+            //*/
         }
     }
 
@@ -41,64 +41,58 @@ var ARISJS = function()
         this.dequeueRequest();
     }
 
-    this.closeMe = function()
-    {
-        this.enqueueRequest("aris://closeMe");
-    }
+    this.closeMe             = function()                { this.enqueueRequest("aris://closeMe"); }
+    this.exitToTab           = function(tab)             { this.enqueueRequest("aris://exitTo/tab/"+tab); }
+    this.exitToPlaque        = function(plaqueId)        { this.enqueueRequest("aris://exitTo/plaque/"+plaqueId); }
+    this.exitToWebpage       = function(webpageId)       { this.enqueueRequest("aris://exitTo/webpage/"+webpageId); }
+    this.exitToItem          = function(itemId)          { this.enqueueRequest("aris://exitTo/item/"+itemId); }
+    this.exitToCharacter     = function(characterId)     { this.enqueueRequest("aris://exitTo/character/"+characterId); }
+    this.exitToPanoramic     = function(panoramicId)     { this.enqueueRequest("aris://exitTo/panoramic/"+panoramicId); }
+    this.prepareMedia        = function(mediaId)         { this.enqueueRequest("aris://media/prepare/" + mediaId); }
+    this.playMedia           = function(mediaId)         { this.enqueueRequest("aris://media/play/" + mediaId); }
+    this.playMediaAndVibrate = function(mediaId)         { this.enqueueRequest("aris://media/playAndVibrate/" + mediaId); }
+    this.stopMedia           = function(mediaId)         { this.enqueueRequest("aris://media/stop/" + mediaId); }
+    this.setMediaVolume      = function(mediaId, volume) { this.enqueueRequest("aris://media/setVolume/" + mediaId + "/" + volume); }
+    this.getItemCount        = function(itemId)          { this.enqueueRequest("aris://inventory/get/" + itemId); }
+    this.setItemCount        = function(itemId,qty)      { this.enqueueRequest("aris://inventory/set/" + itemId + "/" + qty); }
+    this.giveItemCount       = function(itemId,qty)      { this.enqueueRequest("aris://inventory/give/" + itemId + "/" + qty); }
+    this.takeItemCount       = function(itemId,qty)      { this.enqueueRequest("aris://inventory/take/" + itemId + "/" + qty); }
+    this.getPlayerName       = function()                { this.enqueueRequest("aris://player/name"); }
+    this.setBumpString       = function(bString)         { this.enqueueRequest("aris://bump/"+bString); }
 
-    this.prepareMedia = function(mediaId)
+    //Call ARIS API directly (USE WITH CAUTION)
+    this.callService = function(serviceName, callback, GETparams, POSTparams)
     {
-        this.enqueueRequest("aris://media/prepare/" + mediaId);
-    }
-
-    this.playMedia = function(mediaId)
-    {
-        this.enqueueRequest("aris://media/play/" + mediaId);
-    }
-
-    this.playMediaAndVibrate = function(mediaId)
-    {
-        this.enqueueRequest("aris://media/playAndVibrate/" + mediaId);
-    }
-
-    this.stopMedia = function(mediaId)
-    {
-        this.enqueueRequest("aris://media/stop/" + mediaId);
-    }
-
-    this.setMediaVolume = function(mediaId, volume)
-    {
-        this.enqueueRequest("aris://media/setVolume/" + mediaId + "/" + volume);
-    }
-
-    this.getItemCount = function(itemId)
-    {
-        this.enqueueRequest("aris://inventory/get/" + itemId);
-    }
-
-    this.setItemCount = function(itemId,qty)
-    { 
-        this.enqueueRequest("aris://inventory/set/" + itemId + "/" + qty);
-    }
-
-    this.giveItemCount = function(itemId,qty)
-    {
-        this.enqueueRequest("aris://inventory/give/" + itemId + "/" + qty);
-    }
-
-    this.takeItemCount = function(itemId,qty)
-    {
-        this.enqueueRequest("aris://inventory/take/" + itemId + "/" + qty);
-    }
-
-    this.getPlayerName = function()
-    {
-        this.enqueueRequest("aris://player/name");
-    }
-
-    this.setBumpString = function(bString)
-    {
-        this.enqueueRequest("aris://bump/"+bString);
+        var ROOT_URL = "http://arisgames.org"
+        var url;
+        if(GETparams) url = ROOT_URL+'/server/json.php/v1.'+serviceName+GETparams;
+        else          url = ROOT_URL+'/server/json.php/v1.'+serviceName;
+    
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function()
+        {
+            if(request.readyState == 4)
+            {
+                if(request.status == 200)
+                    callback(request.responseText);
+                else
+                    callback(false);
+            }
+        };
+        if(POSTparams)
+        {
+            request.open('POST', url, true);
+            request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+            request.send(POSTparams);
+            console.log("POSTparams:" + POSTparams);
+            console.log("url:" + url);
+        }
+        else
+        {
+            request.open('GET', url, true);
+            request.send();
+            console.log("GETurl:" + url);
+        }
     }
 
     //Not ARIS related... just kinda useful
